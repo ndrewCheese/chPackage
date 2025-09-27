5e5df106ebb2d4329f2815bd32a1733da1826f2a
unkver <unknownversion@tutamail.com>
2025-09-27 13:54:25 +0300

    oopsie, forgot to include installUtils.h in the install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-g++ main.cpp -o chPkg `pkg-config --libs --cflags gtkmm-3.0`
+g++ main.cpp installUtils.h -o chPkg `pkg-config --libs --cflags gtkmm-3.0`
 echo "Done. If you want to open chPkg anywhere, make sure it is in your PATH"
 
 
